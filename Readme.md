52b42b94919be077d8cbdf8d2a1090d43d98f464
Denis Zatsepin <denis@zatsepin.spb.ru>
2014-01-19 13:46:46 +0400

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,18 @@
+
+# wgl-canvas
+
+  &quot;prepare canvas element for webgl context&quot;
+
+## Installation
+
+  Install with [component(1)](http://component.io):
+
+    $ component install deniszatsepin/wgl-canvas
+
+## API
+
+
+
+## License
+
+  MIT
---
+++ index.js
@@ -0,0 +1,33 @@
+module.exports = canvas;
+
+function canvas (id, options) {
+	var options = options || {};
+	if (id) {
+		this.element = document.getElementById(id);
+	} else {
+		this.element = document.createElement('canvas');
+		document.appendChild(this.element);
+	}
+
+	this._width = options.width || 800;
+	this._height = options.height || 600;
+
+}
+
+canvas.prototype.width = function (w) {
+	if (!typeof w) {
+		return this._width;
+	} else {
+		this._width = parseInt(w);
+		this.element.style.width = w + "px";
+	}
+};
+
+canvas.prototype.height = function (h) {
+	if (!typeof h) {
+		return this._height;
+	} else {
+		this._height = parseInt(h);
+		this.element.style.height = h + "px";
+	}
+};
